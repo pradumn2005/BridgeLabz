6b64776fdcee0f11b27db06abde53f0db4b97e30
Pradumn Gupta <gpradumn8@gmail.com>
2025-08-20 16:05:38 +0530

    New file added

--- JAVA/Assignment2/src/DistanceConverter.java
+++ JAVA/Assignment2/src/DistanceConverter.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class DistanceConverter {
     public static void main(String[] args) {
-        double kilometers = 10.8;
-        double milesPerKm = 1.6;
+        Scanner input = new Scanner(System.in);
 
-        double miles = kilometers * milesPerKm;
+        System.out.print("Enter distance in feet: ");
+        double distanceInFeet = input.nextDouble();
 
-        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+        double distanceInYards = distanceInFeet / 3;
+        double distanceInMiles = distanceInYards / 1760;
+
+        System.out.println("The distance in yards is " + distanceInYards +
+                " and in miles is " + distanceInMiles);
     }
 }
