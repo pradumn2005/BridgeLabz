8922c89e6ef7333eb13ee45be6b7b12aecd69d68
Pradumn Gupta <gpradumn8@gmail.com>
2025-08-25 14:24:28 +0530

    New file added

---
+++ JAVA/Level3/src/DigitCount.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class DigitCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int count = 0;
+        int temp = number;
+
+        if (temp == 0) count = 1;
+        while (temp != 0) {
+            temp /= 10;
+            count++;
+        }
+
+        System.out.println("Number of digits in " + number + " is " + count);
+        sc.close();
+    }
+}
