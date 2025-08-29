03020f73f8bf84c0d17f37c722fc09755263d52d
Pradumn Gupta <gpradumn8@gmail.com>
2025-08-29 20:36:25 +0530

    New file added

---
+++ JAVA/JAVA ARRAYS/Leve1LabPractice/NumberCheck.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
+        }
+
+        for (int num : numbers) {
+            if (num > 0) {
+                if (num % 2 == 0) {
+                    System.out.println(num + " is positive and even.");
+                } else {
+                    System.out.println(num + " is positive and odd.");
+                }
+            } else if (num < 0) {
+                System.out.println(num + " is negative.");
+            } else {
+                System.out.println(num + " is zero.");
+            }
+        }
+
+        if (numbers[0] == numbers[4]) {
+            System.out.println("First and last numbers are equal.");
+        } else if (numbers[0] > numbers[4]) {
+            System.out.println("First number is greater than last.");
+        } else {
+            System.out.println("First number is smaller than last.");
+        }
+    }
+}
