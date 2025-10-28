e645137d8cf25d6d9a94c37ef43a41eee30ee2a1
Pradumn Gupta <gpradumn8@gmail.com>
2025-10-28 13:56:07 +0530

    Add files via upload

---
+++ JAVA OOPS/Package Assignment/Project 4/Modular Demo/app/src/MainApp.java
@@ -0,0 +1,8 @@
+import college.student.Student;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Student s1 = new Student("Pradumn Gupta", 101);
+        s1.displayInfo();
+    }
+}
